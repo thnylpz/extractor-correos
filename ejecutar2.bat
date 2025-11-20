dd0574dfb009e6bf904006bb8273c3ba7846458d
Anthony López R. <anthxny.lopezr@gmail.com>
2025-11-20 10:14:38 -0500

    Enhance ejecutar2.bat with installation messages
    Added messages for dependency installation and cleared the screen before running the main script.

--- ejecutar2.bat
+++ ejecutar2.bat
@@ -3,24 +3,27 @@
 REM --- Ir a la carpeta donde está el BAT ---
 cd /d "%~dp0"
 
+REM --- Actualizar desde Github ---
 git pull
 
 REM --- Verificar si Python está instalado ---
-
 python --version >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
-    echo ERROR: Python no está instalado en esta PC.
+    echo ERROR: Python no esta instalado en esta PC.
     echo Por favor instala Python desde: https://www.python.org/downloads/
     pause
     exit /b
 )
 
 REM --- Instalar dependencias ---
+echo Instalando dependencias
+echo.
 IF EXIST requirements.txt (
     python -m pip install --upgrade pip >nul
     python -m pip install -r requirements.txt
 )
 
+cls
+
 REM --- Ejecutar el script principal ---
 python extractor_de_correos.py
-pause
