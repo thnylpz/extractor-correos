f1aaa8da280f9b101257c47cf5dbf4b3b355264e
Anthony López R. <anthxny.lopezr@gmail.com>
2025-11-20 11:24:34 -0500

    Fix shortcut script paths and cleanup
    Updated the script to reference the correct path for 'ejecutar.bat' and added cleanup for the VBS file.

--- Crear Acceso Directo.bat
+++ Crear Acceso Directo.bat
@@ -1,15 +1,15 @@
 @echo off
 
 REM ====== Rutas ======
-set TARGET=%~dp0ejecutar.bat
-set ICON=%~dp0icono_mail.ico
+set TARGET=%~dp0\scripts\ejecutar.bat
+set ICON=%~dp0\icon\icono_mail.ico
 set SHORTCUT_PATH=%USERPROFILE%\Desktop\Extractor de correos.lnk
 set VBS=%TEMP%\shortcut.vbs
 
 echo Set oWS = CreateObject("WScript.Shell") > "%VBS%"
 echo sLinkFile = "%USERPROFILE%\Desktop\Extractor de correos.lnk" >> "%VBS%"
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> "%VBS%"
-echo oLink.TargetPath = "%CD%\ejecutar.bat" >> "%VBS%"
+echo oLink.TargetPath = "%CD%\scripts\ejecutar.bat" >> "%VBS%"
 echo oLink.WorkingDirectory = "%CD%" >> "%VBS%"
 echo oLink.IconLocation = "%CD%\icon\icono_mail.ico" >> "%VBS%"
 echo oLink.Save >> "%VBS%"
