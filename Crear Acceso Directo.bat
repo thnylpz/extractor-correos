d61304cbe6e579b53bb105c179b993074500404b
Your Name <you@example.com>
2025-11-24 10:19:29 -0500

    aceptar Users con caratecres especiales

--- Crear Acceso Directo.bat
+++ Crear Acceso Directo.bat
@@ -4,15 +4,36 @@ REM ====== Rutas ======
 set TARGET=%~dp0\scripts\ejecutar.bat
 set ICON=%~dp0\icons\icono_mail.ico
 set SHORTCUT_PATH=%USERPROFILE%\Desktop\Extractor de correos.lnk
-set VBS=%TEMP%\shortcut.vbs
+@REM set VBS=%TEMP%\shortcut.vbs
 
-echo Set oWS = CreateObject("WScript.Shell") > "%VBS%"
-echo sLinkFile = "%USERPROFILE%\Desktop\Extractor de correos.lnk" >> "%VBS%"
-echo Set oLink = oWS.CreateShortcut(sLinkFile) >> "%VBS%"
-echo oLink.TargetPath = "%CD%\scripts\ejecutar.bat" >> "%VBS%"
-echo oLink.WorkingDirectory = "%CD%" >> "%VBS%"
-echo oLink.IconLocation = "%CD%\icons\icono_mail.ico" >> "%VBS%"
-echo oLink.Save >> "%VBS%"
+@REM Convertir rutas a formato PowerShell seguro
+for %%A in ("%TARGET%") do set "PTARGET=%%~fA"
+for %%A in ("%ICON%") do set "PICON=%%~fA"
+for %%A in ("%SHORTCUT_PATH%") do set "PSHORTCUT=%%~fA"
 
-cscript //nologo "%VBS%"
-del "%VBS%"
+@REM @echo Creando acceso directo...
+
+powershell -NoProfile -Command ^
+    "$W = New-Object -ComObject WScript.Shell; " ^
+    "$S = $W.CreateShortcut('%PSHORTCUT%'); " ^
+    "$S.TargetPath = '%PTARGET%'; " ^
+    "$S.WorkingDirectory = (Split-Path '%PTARGET%'); " ^
+    "$S.IconLocation = '%PICON%'; " ^
+    "$S.Save()"
+
+@REM cls
+@echo Acceso directo creado correctamente.
+@echo.
+
+@REM echo Set oWS = CreateObject("WScript.Shell") > "%VBS%"
+@REM echo sLinkFile = "%USERPROFILE%\Desktop\Extractor de correos.lnk" >> "%VBS%"
+@REM echo Set oLink = oWS.CreateShortcut(sLinkFile) >> "%VBS%"
+@REM echo oLink.TargetPath = "%CD%\scripts\ejecutar.bat" >> "%VBS%"
+@REM echo oLink.WorkingDirectory = "%CD%" >> "%VBS%"
+@REM echo oLink.IconLocation = "%CD%\icons\icono_mail.ico" >> "%VBS%"
+@REM echo oLink.Save >> "%VBS%"
+
+@REM cscript //nologo "%VBS%"
+@REM del "%VBS%"
+
+pause
