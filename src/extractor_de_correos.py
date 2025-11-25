3db9f6bb02ff1dbc389e7225b956da5a15992f97
Anthony López R. <anthxny.lopezr@gmail.com>
2025-11-25 09:08:24 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- src/extractor_de_correos.py
+++ src/extractor_de_correos.py
@@ -315,7 +315,7 @@ def exportar_excel(registros, carpeta_base, fecha_inicio_str):
         os.system("cls")
         os.startfile(carpeta_base)
     else:
-        print(Fore.YELLOW + "\n\nNo se encontraron Correos en el rango especificado.")
+        print(Fore.RED + "\n\nNo se encontraron Correos en el rango especificado.")
         input("\nPresione ENTER para continuar.")
         os.system("cls")    
 
@@ -614,4 +614,5 @@ PERSONAL_LIMPIO = {
 if __name__ == "__main__":
     while True:
         procesar()
+
 
